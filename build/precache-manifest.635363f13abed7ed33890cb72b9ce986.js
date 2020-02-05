a111e38341e6332a84ce1cf95c7d43993c3385a3
Ahsan-Rahim <Ahsan.Rahim@ibm.com>
2020-02-05 09:30:46 -0600

    added build

---
+++ build/precache-manifest.635363f13abed7ed33890cb72b9ce986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129779ddac29d5a2df05dc8e85f965af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "552660bdfd1ef60d3a57",
+    "url": "/static/css/main.2ced01d8.chunk.css"
+  },
+  {
+    "revision": "a0f0a8881f61db18dcb4",
+    "url": "/static/js/2.b2ece36f.chunk.js"
+  },
+  {
+    "revision": "552660bdfd1ef60d3a57",
+    "url": "/static/js/main.9cdd801f.chunk.js"
+  },
+  {
+    "revision": "e3d3ac4aa49918089f4b",
+    "url": "/static/js/runtime-main.23ddeaa0.js"
+  }
+]);
